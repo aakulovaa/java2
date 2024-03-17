da519c6a1fd386c2ee453cdff82c9fa4eeaaed4c
annakulova <aakulovaa03@gmail.com>
2024-03-17 16:44:28 +0300

    Добавление тестов

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,14 @@
 package org.example;
+import java.util.Scanner;
 
 public class App
 {
     public static void main( String[] args )
     {
+        Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        calculator.calculate("  2*5 -   36 / 6"); //4
-        calculator.calculate("12 * 5 - 36 / 3"); //48
-        calculator.calculate("  1 + 2*5 -   3+6 / 6");//9
+        System.out.println("Введите выражение:");
+        String expression = scanner.nextLine();
+        calculator.calculate(expression);
     }
 }
---
+++ src/test/java/org/example/LexerTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LexerTest {
+
+    @Test
+    public void testMethodThrowsException() {
+        boolean exceptionThrown = false;
+        Lexer lexer = new Lexer();
+        try {
+            lexer.getTokens("12 + 2 / 7 &");
+        } catch (RuntimeException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+}
