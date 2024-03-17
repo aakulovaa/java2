47d23af0792dd1809486ab123a3929441ea7c94b
annakulova <aakulovaa03@gmail.com>
2024-03-17 13:02:15 +0300

    Lexer
    Создание класса для лексического анализа

---
+++ src/main/java/org/example/Lexer.java
@@ -0,0 +1,58 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Lexer {
+    private final static String DELIMITERS = " +-*/()";
+
+    /**
+     * Метод разбиения строки на отдельные лексемы, используя определенный набор разделителей.
+     * @param source - строка для обработки, состоящая из последовательности символов
+     * @return - возвращает список токенов в порядке исходного выражения
+     */
+    public List<Token> getTokens(String source){
+        StringTokenizer tokenizer = new StringTokenizer(source, DELIMITERS, true);
+        List<Token> tokens = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            if (token.isBlank())
+            {
+                continue;
+            }else if (isNumber(token)){
+                tokens.add(new NumberToken(Integer.parseInt(token)));
+                continue;
+            }else {
+                tokens.add(
+                        switch (token) {
+                            case "+" -> new BinaryOperationToken(OperationType.PLUS);
+                            case "-" -> new BinaryOperationToken(OperationType.MINUS);
+                            case "*" -> new BinaryOperationToken(OperationType.MULTIPLY);
+                            case "/" -> new BinaryOperationToken(OperationType.DIVIDE);
+                            case "(" -> new ParenthesesToken(TokenType.OPEN_BRACKET);
+                            case ")" -> new ParenthesesToken(TokenType.CLOSE_BRACKET);
+                            default -> throw new RuntimeException("Unexpected token: " + token);
+                        }
+                );
+            }
+
+        }
+        return tokens;
+    }
+
+    /**
+     * Метод для проверки тго, что токен является числом
+     * @param token - строка, подаваемая для проверки
+     * @return - возвращает false, если символ по текущему индуксу не является цифрой,
+     * если полный проход по циклу не вернул false, значит все символы в токене являются цифрами,
+     * тогда вернем true
+     */
+    private boolean isNumber(String token){
+        for (int symbol=0;symbol<token.length();symbol++){
+            if (!Character.isDigit(token.charAt(symbol))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
