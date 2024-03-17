1dc8595cd7898e564acc4aba09790bb08fe5e568
annakulova <aakulovaa03@gmail.com>
2024-03-17 14:57:45 +0300

    Вычисление на стековой машине.
    Создание класса, вычисляющего выражение

---
+++ src/main/java/org/example/StackMachine.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.List;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class StackMachine {
+    /**
+     * Метод вычисления на стековой машине.
+     * @param postfixExpression - выражение, записанное в обратной польской нотации
+     * @return - возвращает результат вычисления заданного выражения
+     */
+    public int evaluate(List<Token> postfixExpression){
+        Deque<Integer> valueStack = new LinkedList<>();
+        for (Token token : postfixExpression) {
+            if (token instanceof  NumberToken number) {
+                valueStack.push(number.value);
+            }else if (token instanceof BinaryOperationToken operationToken){
+                Integer right = valueStack.pop();
+                Integer left = valueStack.pop();
+                Integer result = switch (operationToken.getOperationType()){
+                    case PLUS -> left + right;
+                    case MINUS -> left - right;
+                    case MULTIPLY -> left * right;
+                    case DIVIDE -> {
+                        if (right == 0){
+                            throw new RuntimeException("Divide by zero");
+                        }
+                        else{
+                            yield left/right;
+                        }
+                    }
+                };
+                valueStack.push(result);
+            }
+        }
+        return valueStack.pop();
+    }
+}
