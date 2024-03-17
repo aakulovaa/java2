38b4522f54fae9ce8ddd323ee1bfe0f6a549ad1d
annakulova <aakulovaa03@gmail.com>
2024-03-17 17:23:59 +0300

    Избавление от предупреждений

--- src/test/java/org/example/LexerTest.java
+++ src/test/java/org/example/LexerTest.java
@@ -2,11 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class LexerTest {
     @Test
